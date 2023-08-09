8947de4eacbec65b650bce0de16a187da41f9bfc
Clare Gibson <clarelgibson@gmail.com>
2023-08-09 23:55:48 +0100

    Updates code

--- R/1-get-data.R
+++ R/1-get-data.R
@@ -3,14 +3,86 @@
 
 # SETUP ########################################################################
 library(here)
-library(httr)
+library(xml2)
+library(tidyr)
+library(janitor)
+library(readr)
+library(dplyr)
+library(glue)
+library(sf)
+
+# UK LOCAL AUTHORITY DISTRICTS #################################################
+# > FHR LADs ===================================================================
+fhrlad_url <- "https://ratings.food.gov.uk/authorities/xml"
+fhrlad_xml <- as_list(read_xml(fhrlad_url))
+
+# Convert to df
+fhrlad_src <- 
+  as_tibble(fhrlad_xml) %>% 
+  # split data into columns
+  unnest_wider(ArrayOfWebLocalAuthorityAPI) %>% 
+  # tidy column headings
+  clean_names() %>% 
+  # unnest twice to get the data out of list format
+  unnest(cols = names(.)) %>% 
+  unnest(cols = names(.)) %>% 
+  # remove country level records
+  filter(!is.na(friendly_name)) %>% 
+  # clean data types
+  type_convert()
+
+# > UK Geography LADs ==========================================================
+ukglad_path <- here("data/src/shape-lad")
+ukglad_src <- read_sf(ukglad_path)
 
 # FHR API ######################################################################
-url <- "www.food.gov.uk/ratings"
+# List LA codes to loop through and extract data
+lad_codes <- 
+  fhrlad_src %>% 
+  # keep only records for England and Wales
+  filter(!(region_name %in% c("Scotland", "Northern Ireland"))) %>% 
+  select(file = file_name,
+         code = local_authority_id_code,
+         name)
 
-res <- VERB(
-  "GET",
-  url = url
-)
+# Use smaller number of values as test data
+#lad_codes <- lad_codes[1:10, ]
 
-content <- content(res, "text")
+# Create an empty df to store the results
+fhr_src <- tibble()
+
+# Loop through each LAD code, extract data and store in df
+if (!file.exists(here("data/src/fhr_src.rds"))) {
+  
+  for (i in seq_along(1:nrow(lad_codes))) {
+    print(glue("Starting iteration {i} of {nrow(lad_codes)} \n"))
+    print(glue("..Local authority {lad_codes$code[i]}: {lad_codes$name[i]} \n"))
+    
+    # Read data for this iteration
+    url <- lad_codes$file[i]
+    xml <- as_list(read_xml(url))
+    
+    # Get iteration data in to df
+    df <- 
+      as_tibble(xml) %>% 
+      unnest_longer(FHRSEstablishment) %>% 
+      filter(FHRSEstablishment_id == "EstablishmentDetail") %>% 
+      # unnest all levels with children
+      unnest_wider(FHRSEstablishment) %>% 
+      unnest_wider(Scores) %>% 
+      unnest_wider(Geocode) %>% 
+      # unnest twice to extract values from list
+      unnest(cols = names(.)) %>% 
+      unnest(cols = names(.))
+    
+    # Bind data to df
+    fhr_src <- fhr_src %>% 
+      bind_rows(df)
+    
+    print(glue("Finished iteration {i} of {nrow(lad_codes)} \n"))
+  } 
+  
+  saveRDS(fhr_src, file = here("data/src/fhr_src.rds"))
+} else {
+  fhr_src <- readRDS(here("data/src/fhr_src.rds"))
+}
---
+++ R/2-prep-data.R
@@ -0,0 +1,183 @@
+# SUMMARY ######################################################################
+# This script cleans the data ready for further analysis in Tableau
+
+# SETUP ########################################################################
+# > Packages ===================================================================
+library(here)
+
+# > Scripts ====================================================================
+source(here("R/1-get-data.R"))
+
+# CLEAN DATA ###################################################################
+# > LADs =======================================================================
+# Which LADs in the UKG data have no match in the FHR data?
+ukglad_anti <- ukglad_src %>% 
+  clean_names() %>% 
+  select(lad23nm) %>% 
+  anti_join(
+    select(
+      fhrlad_src,
+      lad23nm = name
+    )
+  )
+
+# Add clean names to the FHR LAD data so that we can join UKG data
+fhrlad_cln <- 
+  fhrlad_src %>% 
+  mutate(
+    lad23nm = case_when(
+      name == "Stockton On Tees" ~ "Stockton-on-Tees",
+      name == "Blackburn" ~ "Blackburn with Darwen",
+      name == "Hull City" ~ "Kingston upon Hull, City of",
+      name == "Hull and Goole Port" ~ "Kingston upon Hull, City of",
+      name == "Derby City" ~ "Derby",
+      name == "Leicester City" ~ "Leicester",
+      name == "Nottingham City" ~ "Nottingham",
+      name == "Herefordshire" ~ "Herefordshire, County of",
+      name == "Telford and Wrekin Council" ~ "Telford and Wrekin",
+      name == "Stoke-On-Trent" ~ "Stoke-on-Trent",
+      name == "Bristol" ~ "Bristol, City of",
+      name == "Plymouth City" ~ "Plymouth",
+      name == "Peterborough City" ~ "Peterborough",
+      name == "Southend-On-Sea" ~ "Southend-on-Sea",
+      name == "Durham" ~ "County Durham",
+      name == "Cambridge City" ~ "Cambridge",
+      name == "Exeter City" ~ "Exeter",
+      name == "Gloucester City" ~ "Gloucester",
+      name == "Winchester City" ~ "Winchester",
+      name == "Canterbury City" ~ "Canterbury",
+      name == "Lancaster City" ~ "Lancaster",
+      name == "Lincoln City" ~ "Lincoln",
+      name == "Norwich City" ~ "Norwich",
+      name == "Oxford City" ~ "Oxford",
+      name == "Newcastle-Under-Lyme" ~ "Newcastle-under-Lyme",
+      name == "Worcester City" ~ "Worcester",
+      name == "St Albans City" ~ "St Albans",
+      name == "St Helens" ~ "St. Helens",
+      name == "Newcastle Upon Tyne" ~ "Newcastle upon Tyne",
+      name == "City of London Corporation" ~ "City of London",
+      name == "Kingston-Upon-Thames" ~ "Kingston upon Thames",
+      name == "Richmond-Upon-Thames" ~ "Richmond upon Thames",
+      name == "Belfast City" ~ "Belfast",
+      name == "Lisburn and Castlereagh City" ~ "Lisburn and Castlereagh",
+      name == "Comhairle nan Eilean Siar (Western Isles)" ~ "Na h-Eileanan Siar",
+      name == "Edinburgh (City of)" ~ "City of Edinburgh",
+      name == "Anglesey" ~ "Isle of Anglesey",
+      name == "River Tees" ~ "Redcar and Cleveland",
+      TRUE ~ name
+    )
+  )
+
+# Which LADs in the FHR data have no match in the UKG data?
+fhrlad_anti <- fhrlad_cln %>% 
+  clean_names() %>% 
+  select(lad23nm) %>% 
+  anti_join(
+    select(
+      ukglad_src,
+      lad23nm = LAD23NM
+    )
+  )
+
+# Make full dataframe for Local Authorities
+lad <- ukglad_src %>% 
+  clean_names() %>% 
+  full_join(fhrlad_cln,
+            by = "lad23nm") %>% 
+  rename(local_authority_code = local_authority_id_code)
+
+# > FHR ========================================================================
+fhr <- fhr_src %>% 
+  # Clean the column headers
+  clean_names() %>% 
+  # Remove records with missing location data
+  filter(!is.na(longitude)) %>% 
+  # clean rating value
+  mutate(rating_value = as.numeric(rating_value)) %>% 
+  # Remove records with missing rating values
+  filter(!is.na(rating_value)) %>% 
+  # recode rating key
+  mutate(
+    rating_key = case_when(
+      grepl("0", rating_key) ~ "Urgent improvement is required",
+      grepl("1", rating_key) ~ "Major improvement is required",
+      grepl("2", rating_key) ~ "Some improvement is required",
+      grepl("3", rating_key) ~ "Hygiene standards are generally satisfactory",
+      grepl("4", rating_key) ~ "Hygiene standards are good",
+      grepl("5", rating_key) ~ "Hygiene standards are very good",
+      TRUE ~ rating_key
+    )
+  ) %>% 
+  # add rating indicators
+  mutate(
+    rating_indicator_inc_zero = case_when(
+      rating_value >= 3 ~ "Satisfactory",
+      rating_value >= 1 ~ "Unsatisfactory",
+      rating_value == 0 ~ "Rating 0"
+    ),
+    rating_indicator_binary = case_when(
+      rating_value >= 3 ~ "Satisfactory",
+      rating_value >= 0 ~ "Unsatisfactory"
+    )
+  ) %>% 
+  # append score to score columns
+  rename(hygiene_score = hygiene,
+         structural_score = structural,
+         management_score = confidence_in_management) %>% 
+  # join LAD data
+  left_join(lad,
+            by = "local_authority_code") %>% 
+  # select and rename columns to keep
+  select(
+    fhrsid,
+    business_id = local_authority_business_id,
+    business_name,
+    business_type,
+    business_longitude = longitude,
+    business_latitude = latitude,
+    rating_value,
+    rating_key,
+    rating_date,
+    rating_indicator_inc_zero,
+    rating_indicator_binary,
+    local_authority_code,
+    local_authority_name,
+    local_authority_web_site,
+    local_authority_longitude = long,
+    local_authority_latitude = lat,
+    region_name,
+    geometry
+  ) %>% 
+  # correct data types
+  type_convert()
+
+# > FHR Waffle =================================================================
+fhr_waffle <- fhr %>% 
+  # select dimensions to measure on
+  select(local_authority_name,
+         business_type,
+         rating_indicator_binary) %>% 
+  arrange(local_authority_name,
+          business_type,
+          rating_indicator_binary) %>% 
+  # add count by business type
+  group_by(local_authority_name,
+           business_type) %>% 
+  mutate(count_business_type = n()) %>% 
+  ungroup() %>% 
+  # add count of rating by business type and local authority
+  group_by(local_authority_name,
+           business_type,
+           rating_indicator_binary) %>% 
+  mutate(count_rating = n(),
+         pct_rating = count_rating / count_business_type) %>% 
+  ungroup() %>% 
+  distinct() %>% 
+  # keep only the satisfactory rows
+  filter(rating_indicator_binary == "Satisfactory") %>% 
+  select(local_authority_name,
+         business_type,
+         total_businesses = count_business_type,
+         count_satisfactory = count_rating,
+         pct_satisfactory = pct_rating)
+
